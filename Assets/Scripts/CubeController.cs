d45ddcc808ae57c977fad91a691e32afd4378ff2
ActiveNick <nick.landry@microsoft.com>
2019-11-18 07:25:47 -0500

    Initial Code Commit

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(AudioSource))]
+public class CubeController : MonoBehaviour
+{
+    public bool isRotating = false;
+    public float rotAngle = 1.0f;
+    public Material colorSuccess;
+
+    AudioSource chime;
+    Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chime = GetComponent<AudioSource>();
+        body = GetComponent<Rigidbody>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRotating)
+        {
+            transform.Rotate(Vector3.up, rotAngle);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            chime.Play();
+            body.useGravity = true;
+            isRotating = false;
+            gameObject.GetComponent<MeshRenderer>().material = colorSuccess;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Pickup object
+    public GameObject spawn;
+    public AudioClip soundSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnNewPickup", 3.0f, 5.0f);
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    void SpawnNewPickup()
+    {
+        float randX = Random.Range(-20, 20);
+        float randZ = Random.Range(-20, 20);
+
+        var newpickup = GameObject.Instantiate(spawn, new Vector3(randX, 1, randZ), Quaternion.identity);
+        AudioSource.PlayClipAtPoint(soundSpawn, newpickup.transform.position);
+    }
+}
